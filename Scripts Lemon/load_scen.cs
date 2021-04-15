4bb4195904b8382a849e6de834f7122440f65233
ZolotukhinEduard <80178083+ZolotukhinEduard@users.noreply.github.com>
2021-04-16 00:06:26 +0300

    Skript Lemon

---
+++ Scripts Lemon/load_scen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class load_scen : MonoBehaviour
+{
+
+    public void LoadScene (int Level)
+    {
+        SceneManager.LoadScene(Level);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
